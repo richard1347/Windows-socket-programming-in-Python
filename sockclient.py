33ad30757bb634e4de4a8d225e4d593fb13f35e2
richard1347 <143375839+richard1347@users.noreply.github.com>
2023-10-20 23:09:49 +0800

    Add files via upload

---
+++ sockclient.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 17:10:19 2023
+
+@author: dell
+"""
+
+import socket,win32api
+
+class ClientSocket:
+    peerIpaddr=''
+    peerPort=0
+    def __init__(self,ip,port):
+        self.peerIpaddr=ip
+        self.peerPort=port
+    def connect_server(self):
+        connSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        connSocket.connect((self.peerIpaddr,self.peerPort))
+        win32api.MessageBox(0,'Already connected server at '+str(self.peerIpaddr)+':'+str(self.peerPort),'Connecting Result',0x00|0x40)
+        while True:
+            #sending=input('Please input your message:>')
+            sending='requesting'
+            if sending=='q' or sending=='exit':
+                break;
+            connSocket.send(sending.encode())
+            recved=connSocket.recv(512)
+            if not recved:
+                break
+            print(recved.decode())
+        connSocket.close()
+        
+if __name__=='__main__':
+    sock1=ClientSocket('192.168.0.5',23234)
+    sock1.connect_server()
---
+++ sockserver.py
@@ -0,0 +1,32 @@
+import socket,win32api
+from socket import *
+
+class ServerSocket:
+    svrIpaddr=''
+    svrPort=0
+    def __init__(self,ip,port):
+        self.svrIpaddr=ip
+        self.svrPort=port
+
+    def start_server(self):
+        svrSocket=socket(AF_INET,SOCK_STREAM)
+        svrSocket.bind((self.svrIpaddr,self.svrPort))
+        svrSocket.listen(100)
+        win32api.MessageBox(0,'Already started server at '+str(self.svrIpaddr)+':'+str(self.svrPort),'Start Server',0x00|0x40)
+        connSocket,peerAddr=svrSocket.accept()
+        print('Accept a client\'s connection from ',peerAddr)
+
+        while True:
+            recved=connSocket.recv(512)
+            if not recved:
+                break
+            data=recved.decode()
+            print(f'Received peer information: {data}')
+            reply=input('What do you want to reply?>')
+            connSocket.send(reply.encode())
+        connSocket.close()
+        svrSocket.close()
+
+if __name__=='__main__':
+    sock1=ServerSocket('192.168.0.6',23234)
+    sock1.start_server()
